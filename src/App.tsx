8576f79bb708d0d0924f8851cf4dcad1b5cc37c0
ANCHANIK <cksdlr7446@gmail.com>
2023-07-25 14:13:22 +0000

    refactor: 컴포넌트 호출

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import Login from "@pages/Login";
+import SignUp from "@pages/SignUp";
+import { postFetcher } from "@utils/fetcher";
+
 const App = () => {
-    return <div>Hello World!!!!</div>;
+    console.log(process.env);
+    const dummy = {
+        id: 'cksdlr7446',
+        pw: 3016
+    }
+    const data = postFetcher('api/v1/public/user/login', dummy)
+    console.log(data);
+    
+
+    return (
+        <>
+            <Login />
+            <SignUp />
+        </>
+    )
 }
 
 export default App;
