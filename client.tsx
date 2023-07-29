cae01f8b1d4ccbd58eb9eb073c1d7ad05376cd03
ANCHANIK <cksdlr7446@gmail.com>
2023-07-29 14:16:40 +0000

    feat: routes 적용

--- client.tsx
+++ client.tsx
@@ -1,7 +1,12 @@
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./src/App";
 
 const root = ReactDOM.createRoot(
     document.querySelector('#app') as HTMLElement
 );
-root.render( <App /> );
+root.render( 
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+);
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
+import Navigation from "@components/Navigation";
 import Login from "@pages/Login";
+import Main from "@pages/Main";
 import SignUp from "@pages/SignUp";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
 
 
     return (
         <>
-            <Login />
-            <SignUp />
+            <Navigation />
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/main" element={<Main />} />
+            </Routes>
         </>
     )
 }
