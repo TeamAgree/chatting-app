04c971b170b8ddc620128de216965820dcc7708e
ANCHANIK <cksdlr7446@gmail.com>
2023-07-31 00:24:15 +0000

    feat: 로그인 component 생성

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,23 +1,46 @@
-import { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import { postFetcher } from "@utils/fetcher";
+import useInput from "@hooks/useInput";
+import { Form } from "./styles";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+type LoginProps = {
+    onSubmit: (
+        form: {
+            id: string;
+            password: string;
+        }
+    ) => void;
+};
 
 const Login = () => {
 
-    const dummy = {
-        id: 'cksdlr7446',
-        pw: 3016
-    }
+    const [ id, onChnageId ] = useInput('');
+    const [ password, onChangePW ] = useInput('');
 
-    const submit = useCallback(() => {
-        const data = postFetcher('api/v1/public/user/login', dummy);
-        console.log(data);
+    const onSubmit = useCallback((e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const data = { id, pw: password };
+        
+        axios.post('/api/v1/public/user/login', data);
         
         
-    }, [])
+    }, [id, password])
 
-    return <>
-        <button onClick={submit}>로그인</button>
-    </>
+    
+
+    return (
+        <section id="container">
+            <Form onSubmit={onSubmit}>
+                <input type="text" name="id" placeholder="ID" value={id} onChange={onChnageId} />
+                <input type="password" name="password" placeholder="PASSWORD" value={password} onChange={onChangePW}/>
+                <button type="submit">로그인</button>
+            </Form>
+            <Link to="/signup">회원가입</Link>
+        </section>
+    )
 }
 
 export default Login;
