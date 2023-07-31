42db759175d58ba789b9d13dbbf0831f5b408e87
ANCHANIK <cksdlr7446@gmail.com>
2023-07-31 00:26:58 +0000

    feat: workspace component 생성

---
+++ src/layouts/workspace/index.tsx
@@ -0,0 +1,12 @@
+import Navigation from "@components/Navigation";
+
+const Workspace = () => {
+
+    return (
+        <>
+            <Navigation />
+        </>
+    )
+}
+
+export default Workspace;
