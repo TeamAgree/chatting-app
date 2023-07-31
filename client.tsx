bcfdfb61a36bb477e24ebd72d4ed746e5a30b748
ANCHANIK <cksdlr7446@gmail.com>
2023-07-31 04:26:33 +0000

    feat: recoilRoot 최상단 component 적용

--- client.tsx
+++ client.tsx
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "@layouts/App/App";
 import axios from "axios";
+import { RecoilRoot } from "recoil";
 
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = process.env.NODE_ENV === 'production' ? '' : 'http://34.64.251.46:8088';
@@ -12,6 +13,8 @@ const root = ReactDOM.createRoot(
 );
 root.render( 
     <BrowserRouter>
-        <App />
+        <RecoilRoot>
+            <App />
+        </RecoilRoot>
     </BrowserRouter>
 );
