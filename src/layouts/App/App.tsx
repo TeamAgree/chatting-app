52fd58ac50f8703f7a54309fc1064f5d95cabe2e
ANCHANIK <cksdlr7446@gmail.com>
2023-07-31 04:29:01 +0000

    feat: global css 적용

--- src/layouts/App/App.tsx
+++ src/layouts/App/App.tsx
@@ -2,12 +2,14 @@ import Workspace from "@layouts/workspace";
 import Login from "@pages/Login";
 import SignUp from "@pages/SignUp";
 import { Navigate, Route, Routes } from "react-router-dom";
+import { GlobalStyle } from "./style";
 
 const App = () => {
     const user = true;
 
     return (
         <>
+            <GlobalStyle />
             <Routes>
                 <Route path="/" element={<Navigate to="/login" replace={true}/>}></Route>
                 <Route path="/login" element={<Login />} />
