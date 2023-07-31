97c22156ccb902a491d8ec3ca4de524edba092fb
ANCHANIK <cksdlr7446@gmail.com>
2023-07-31 00:28:12 +0000

    refactor: axios baseUrl 설정

--- client.tsx
+++ client.tsx
@@ -1,6 +1,11 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./src/App";
+import App from "@layouts/App/App";
+import axios from "axios";
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = process.env.NODE_ENV === 'production' ? '' : 'http://34.64.251.46:8088';
+
 
 const root = ReactDOM.createRoot(
     document.querySelector('#app') as HTMLElement
