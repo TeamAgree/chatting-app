71376e1a66e93ac7aeaa2fd70cdec9548500b50f
ANCHANIK <cksdlr7446@gmail.com>
2023-08-01 15:34:59 +0000

    refactor: root 파일 component 상단 axios 설정

--- client.tsx
+++ client.tsx
@@ -4,9 +4,9 @@ import App from "@layouts/App/App";
 import axios from "axios";
 import { RecoilRoot } from "recoil";
 
-axios.defaults.withCredentials = true;
 axios.defaults.baseURL = process.env.NODE_ENV === 'production' ? '' : 'http://34.64.251.46:8088';
-
+axios.defaults.headers['Access-Control-Allow-Origin'] = 'http://34.64.251.46:8082';
+axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(
     document.querySelector('#app') as HTMLElement
