603c389f9d1f202427654a4c7d5e1236e9d94ab7
ANCHANIK <cksdlr7446@gmail.com>
2023-08-01 15:36:28 +0000

    style: login, signup component 스타일

--- src/pages/Login/styles.tsx
+++ src/pages/Login/styles.tsx
@@ -15,7 +15,23 @@ export const LoginWrap = styled.section `
         background: #fff;
         box-shadow: 0px 0px 5px 0px #54545466;
     }
-    
+`
+
+export const SignUpWrap = styled.section`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #eee;
+
+    div.container {
+        padding: 5rem;
+
+        min-width: 20rem;
+        background: #fff;
+        box-shadow: 0px 0px 5px 0px #54545466;
+    }
 `
 
 export const Form = styled.form`
