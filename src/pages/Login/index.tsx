2dcc1c2b7fe9cb18de115c39ddc27abf905880e2
ANCHANIK <cksdlr7446@gmail.com>
2023-08-02 12:04:37 +0000

    refactor: error validation

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,32 +1,64 @@
-import React, { useCallback, useState } from "react";
-import { postFetcher } from "@utils/fetcher";
+import React, { useCallback, useEffect, useState } from "react";
 import useInput from "@hooks/useInput";
 import { LoginWrap, Form } from "./styles";
-import axios from "axios";
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import { LoginProps } from "@typings/db";
 import { customAxios } from "@utils/customAxios";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { UserAtom } from "@recoil/UserAtom";
+import { IsLoginAtom } from "@recoil/IsLoginAtom";
 
 const Login = () => {
 
     const [ id, onChnageId ] = useInput('');
     const [ password, onChangePW ] = useInput('');
+    const [ isError, setIsError ] = useState(false);
+    const [ errorText, setErrorText ] = useState('');
+    const setIsLogin = useSetRecoilState(IsLoginAtom);
+    const isLogin = useRecoilValue(IsLoginAtom);
 
     const onSubmit = useCallback((e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        setIsError(false);
+        setErrorText('');
+
+        if (!id) {
+            setIsError(true);
+            setErrorText('아이디 입력하세요.');
+            return;
+        }
+        if (!password) {
+            setIsError(true);
+            setErrorText('비밀번호 입력하세요.');
+            return;
+        }
 
         const data: LoginProps = {id, pw: password};
         const loginData = async () => {
             const resData = await customAxios('post', '/api/v1/public/user/login', data);
-            
-            console.log(resData);
+            if (resData?.status === 200) {
+                localStorage.setItem("token",resData.data.result);
+                setIsLogin(() => resData.data.result);
+                return <Navigate to="/workspace" replace={true}/>
+            }
         }
         
         loginData();
         
-    }, [id, password])
+    }, [ id, password ]);
 
-    
+    useEffect(() => {
+        setIsError(false);
+        setErrorText('');
+    }, [ id, password ])
+
+    if(isLogin) {
+        console.log(isLogin);
+        
+        return (
+            <Navigate to="/workspace" replace={true} />
+        )
+    }
 
     return (
         <LoginWrap>
@@ -35,6 +67,7 @@ const Login = () => {
                     <input type="text" name="id" placeholder="ID" value={id} onChange={onChnageId} />
                     <input type="password" name="password" placeholder="PASSWORD" value={password} onChange={onChangePW}/>
                     <button type="submit">로그인</button>
+                    {isError && <p>{errorText}</p>}
                 </Form>
                 <Link to="/signup">회원가입</Link>
             </div>
