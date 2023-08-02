7b3dbe1af9d65ceb8767d95570bb752313a34971
ANCHANIK <cksdlr7446@gmail.com>
2023-08-02 06:33:24 +0000

    refactor: IP 주소 변경

--- client.tsx
+++ client.tsx
@@ -4,8 +4,8 @@ import App from "@layouts/App/App";
 import axios from "axios";
 import { RecoilRoot } from "recoil";
 
-axios.defaults.baseURL = process.env.NODE_ENV === 'production' ? '' : 'http://34.64.251.46:8088';
-axios.defaults.headers['Access-Control-Allow-Origin'] = 'http://34.64.251.46:8082';
+axios.defaults.baseURL = process.env.NODE_ENV === 'production' ? '' : 'http://34.22.83.113:8088';
+axios.defaults.headers['Access-Control-Allow-Origin'] = 'http://34.22.83.113:8082';
 axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(
