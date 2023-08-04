38a34637842bf247657679f7c9f0ded07670f14b
ANCHANIK <cksdlr7446@gmail.com>
2023-08-04 13:59:04 +0000

    refactor: axios post, get 분리

--- src/utils/customAxios.ts
+++ src/utils/customAxios.ts
@@ -1,20 +1,29 @@
 import axios, { AxiosRequestConfig } from "axios"
 
-export const customAxios = async ( method: string, url: string, data: {} ) => {
-    const axiosConfig: AxiosRequestConfig = {
+export const customAxios = async ( method: string, url: string, data: {}, token: string | null ) => {
+    const getAxiosConfig: AxiosRequestConfig = {
+        method: method,
+        url: url,
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    };
+    
+    const pushAxiosConfig: AxiosRequestConfig = {
         method: method,
         url: url,
         headers: {
             'Content-Type': 'application/json'
         },
-        data: data
+        data: JSON.stringify(data)
     };
 
     try {
-        const res = await axios.request(axiosConfig);
-        return res;        
+        const res = await axios.request(method === 'post' ? pushAxiosConfig : getAxiosConfig);        
+        return res;
     }
     catch(e) {
-
+        console.error(e);
+        
     }
 }
