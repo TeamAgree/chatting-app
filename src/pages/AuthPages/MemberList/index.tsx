777a119a7719ef39fe59a08ee3754c7f90d14a7e
ANCHANIK <cksdlr7446@gmail.com>
2023-08-10 08:27:40 +0000

    feat: myinfo 컴포넌트 생성

---
+++ src/pages/AuthPages/MemberList/index.tsx
@@ -0,0 +1,34 @@
+import { UserAtom } from "@recoil/UserAtom";
+import { useCallback } from "react";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+
+const MemberList = () => {
+
+    const user = useRecoilValue(UserAtom);
+
+    const memberData = {
+        id: 'cksdlr7446',
+        name: '안찬익',
+        birth: 930316
+    }
+
+    // const onAddMember = useCallback(() => {
+    //     setUser((prev) => [
+    //         ...prev,
+    //         memberData
+    //     ])
+        
+    // }, [user])
+
+
+    return (
+        <div>
+            <button>멤버추가</button>
+            {user.length > 0 && user?.map(v => (
+                v.id
+            ))}
+        </div>
+    )
+}
+
+export default MemberList;
---
+++ src/pages/AuthPages/MyInfo/index.tsx
@@ -0,0 +1,14 @@
+import { useEffect } from "react";
+
+const MyInfo = () => {
+
+    useEffect(() => {
+        // const getUser = 
+    }, [])
+
+    return (
+        <div>My info</div>
+    )
+}
+
+export default MyInfo;
