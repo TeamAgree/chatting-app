17f54ef3b451547c30f5d1d2e09365001539e395
ANCHANIK <cksdlr7446@gmail.com>
2023-08-10 06:14:18 +0000

    feat: Atom Effect를 이용한 localstrage recoil 지속성 적용

---
+++ src/recoil/AccessTokenAtom.ts
@@ -0,0 +1,27 @@
+import { atom, AtomEffect } from "recoil";
+
+const localStorageEffect: <T>(key: string) => AtomEffect<T> =
+    (key: string) => 
+        ({ setSelf, onSet }) => {
+
+    const savedValaue = localStorage.getItem('chatting-app');
+    if (savedValaue != null) {
+        setSelf(JSON.parse(savedValaue));
+    }
+
+    onSet((newValue, _, isReset) => {
+        isReset
+            ? localStorage.removeItem(key)
+            : localStorage.setItem(key, JSON.stringify(newValue));
+    });
+};
+
+export const AccessTokenAtom = atom<string>({
+    key: "AccessTokenAtom",
+    default: "",
+    effects: [
+        localStorageEffect<string>('chatting-app')
+    ]
+});
+
+
