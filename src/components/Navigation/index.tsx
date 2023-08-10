2985e7972b0bb84452560cc554414df81a3fabfd
ANCHANIK <cksdlr7446@gmail.com>
2023-08-10 08:28:53 +0000

    refactor: 네이게이션 내정보 추가

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -6,13 +6,16 @@ const Navigation = () => {
     return (
         <ul>
             <li>
-                <Link to="/workspace/Chatting">채팅 페이지</Link>
+                <Link to="/workspace/chatting">채팅 페이지</Link>
             </li>
             <li>
-                <Link to="/workspace/ChatList">채팅 리스트</Link>
+                <Link to="/workspace/chatList">채팅 리스트</Link>
             </li>
             <li>
-                <Link to="/workspace/MemberList">멤버 리스트</Link>
+                <Link to="/workspace/memberList">멤버 리스트</Link>
+            </li>
+            <li>
+                <Link to="/workspace/myinfo">내 정보</Link>
             </li>
             <li>
                 <Logout />
