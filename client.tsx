f9a11b380660a42738674f711ff51a183dffd161
ANCHANIK <cksdlr7446@gmail.com>
2023-08-10 06:02:34 +0000

    refactor: cookie 제거

--- client.tsx
+++ client.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter } from "react-router-dom";
 import App from "@layouts/App/App";
 import axios from "axios";
 import { RecoilRoot } from "recoil";
-import { CookiesProvider } from "react-cookie";
 
 axios.defaults.baseURL = process.env.NODE_ENV === 'production' ? '' : 'http://34.22.83.113:8088';
 axios.defaults.headers['Access-Control-Allow-Origin'] = 'http://34.22.83.113:8082';
@@ -13,11 +12,9 @@ const root = ReactDOM.createRoot(
     document.querySelector('#app') as HTMLElement
 );
 root.render( 
-    <CookiesProvider>
-        <BrowserRouter>
-                <RecoilRoot>
-                    <App />
-                </RecoilRoot>
-        </BrowserRouter>
-    </CookiesProvider>
+    <BrowserRouter>
+        <RecoilRoot>
+            <App />
+        </RecoilRoot>
+    </BrowserRouter>
 );
