65d5d3273d943e80b117cbb9effcf819d61d5b93
ANCHANIK <cksdlr7446@gmail.com>
2023-08-11 04:14:26 +0000

    refactor: Recoil USER info atom 업데이트

--- src/recoil/UserAtom.ts
+++ src/recoil/UserAtom.ts
@@ -1,14 +1,21 @@
-import { IUser } from "@typings/db";
+import { customAxios } from "@utils/customAxios";
 import { atom, selector } from "recoil";
 
-export const UserAtom = atom<IUser[]>({
-    key: "UserAtom",
-    default: []
+export const _userInfoTrigger = atom<number>({
+    key: "_userInfoTrigger",
+    default: 0
 });
 
 export const UserSelector = selector({
     key: "UserSelector",
     get: async ({ get }) => {
-        const user = get(UserAtom);
+        get(_userInfoTrigger)
+        const user = await customAxios('get', '/api/v1/private/user', null);
+
+        return user;
+    },
+    set: ({ set }) => {
+        set(_userInfoTrigger, v => v + 1);
+        // userInfoTrigger의 값을 1씩 증가시켜 트리거 발동으로 userInfo 갱신
     }
 })
