481065b71bea5c6e8884b860bf0833cfdc51ee4a
ANCHANIK <cksdlr7446@gmail.com>
2023-08-15 09:10:16 +0000

    feat: email 정규식 함수 로직 추가

---
+++ src/utils/validation.ts
@@ -0,0 +1,7 @@
+export const EmailValidation = (value: string) => {
+    const emailRegEx = /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_.]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/;
+    const isEmailCheck = emailRegEx.test(value);
+
+    return isEmailCheck;
+    
+}
