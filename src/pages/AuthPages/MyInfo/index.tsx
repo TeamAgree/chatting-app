133fd5e4159e7db86da272de587465e3b48e1133
ANCHANIK <cksdlr7446@gmail.com>
2023-08-24 14:31:08 +0000

    feat: 내정보 recoil selector axios 연결

--- src/pages/AuthPages/MyInfo/index.tsx
+++ src/pages/AuthPages/MyInfo/index.tsx
@@ -1,6 +1,8 @@
+import { AccessTokenAtom } from "@recoil/AccessTokenAtom";
 import { UserSelector } from "@recoil/UserAtom";
+import axios, { AxiosRequestConfig } from "axios";
 import React, { Suspense, useCallback, useEffect, useState } from "react";
-import { useRecoilValue } from "recoil";
+import { useRecoilStateLoadable, useRecoilValue, useSetRecoilState } from "recoil";
 
 type UserType = {
     id: number;
@@ -8,12 +10,23 @@ type UserType = {
 }
 
 const MyInfo = () => {
-    const getUser = useRecoilValue(UserSelector);
+    
+    const userInfo = useRecoilValue(UserSelector);
+    console.log(userInfo);
+    
+    // const setAccessTokenAtom = useSetRecoilState(AccessTokenAtom);
+    // const [userInfoLoadable, refetchUserInfo] = useRecoilStateLoadable(UserSelector);
+
+    // if (userInfoLoadable.contents.code === 102) {
+    //     setAccessTokenAtom(userInfoLoadable.contents.code.result);
+    // }
+    
+    // console.log(userInfoLoadable);
 
     const onClickUserInfo = useCallback(async (e: React.MouseEvent<HTMLButtonElement>) => {
-        console.log('test', e, getUser);
+        console.log('test', e);
 
-    }, [getUser])
+    }, [])
 
     return (
         <div>
