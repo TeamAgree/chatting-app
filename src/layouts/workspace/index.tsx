671c471762c3a850c6f231b0a36021c29f846d48
ANCHANIK <cksdlr7446@gmail.com>
2023-08-24 14:29:17 +0000

    refactor: Suspense 추가

--- src/layouts/workspace/index.tsx
+++ src/layouts/workspace/index.tsx
@@ -4,27 +4,29 @@ import Chatting from "@pages/AuthPages/Chatting";
 import MemberList from "@pages/AuthPages/MemberList";
 import MyInfo from "@pages/AuthPages/MyInfo";
 import NotFound from "@pages/NotFound";
-import React from "react";
+import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import { HeaderWrap, MainWrap, WorkspaceWrap } from "./styles";
 
 const Workspace = () => {
 
     return (
-        <WorkspaceWrap>
-            <HeaderWrap>
-                <Navigation />
-            </HeaderWrap>
-            <MainWrap>
-                <Routes>
-                    <Route path="/chatting" element={<Chatting />}></Route>
-                    <Route path="/chatList" element={<ChatList />}></Route>
-                    <Route path="/memberList" element={<MemberList />}></Route>
-                    <Route path="/myinfo" element={<MyInfo />}></Route>
-                    <Route path="/*" element={<NotFound />}></Route>
-                </Routes>
-            </MainWrap>
-        </WorkspaceWrap>
+        <Suspense fallback={<p>로딩중...</p>}>
+            <WorkspaceWrap>
+                <HeaderWrap>
+                    <Navigation />
+                </HeaderWrap>
+                <MainWrap>
+                    <Routes>
+                        <Route path="/chatting" element={<Chatting />}></Route>
+                        <Route path="/chatList" element={<ChatList />}></Route>
+                        <Route path="/memberList" element={<MemberList />}></Route>
+                        <Route path="/myinfo" element={<MyInfo />}></Route>
+                        <Route path="/*" element={<NotFound />}></Route>
+                    </Routes>
+                </MainWrap>
+            </WorkspaceWrap>
+        </Suspense>
     )
 }
 
