8896a0bfda177a43b504b946a2f5746ece297d51
ANCHANIK <cksdlr7446@gmail.com>
2023-08-24 14:28:59 +0000

    refactor: nav 순서 변경

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -6,13 +6,13 @@ const Navigation = () => {
     return (
         <ul>
             <li>
-                <Link to="/workspace/chatting">채팅 페이지</Link>
+                <Link to="/workspace/memberList">멤버 리스트</Link>
             </li>
             <li>
                 <Link to="/workspace/chatList">채팅 리스트</Link>
             </li>
             <li>
-                <Link to="/workspace/memberList">멤버 리스트</Link>
+                <Link to="/workspace/chatting">채팅 페이지</Link>
             </li>
             <li>
                 <Link to="/workspace/myinfo">내 정보</Link>
